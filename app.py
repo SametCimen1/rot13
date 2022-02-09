ba65b1262e508304da6e1912d826e7829ad6c882
Samet Cimen <78915841+SametCimen1@users.noreply.github.com>
2022-02-09 11:40:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
 def encrpt(str):
    newStr = ""
    #samet -> s (19 + 26) = 44 % 13 => 5 
    for i in range(len(str)):
-       normalIndex = alphabet.index(str[i]) //26 
-       newIndex = (normalIndex + 13) % 25
-       newStr += alphabet[newIndex]
+       currentIndex = 0
+       for index,letter in enumerate(alphabet):
+           if(letter) == str[i]:
+            currentIndex = index
+       if str[i] == " ":
+            currentIndex  = 26
+            newStr += " "
+       
+       else:
+            newIndex = (currentIndex + 13) % 26
+            newStr += alphabet[newIndex]
+       #18 + 13 -> 31 % 26 -> 5
+
    return newStr
      
     
 
-encrpted = encrpt("samet")
+encrpted = encrpt("hello i am encrypted")
 print(encrpted)
